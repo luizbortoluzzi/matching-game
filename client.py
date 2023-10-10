9a6899f7ef77d36f205695140f712aa30b74b5fc
Luiz Bortoluzzi <57118173+luizbortoluzzi@users.noreply.github.com>
2023-10-10 15:53:20 -0300

    First Commit

---
+++ client.py
@@ -0,0 +1,77 @@
+import xmlrpc.client
+import tkinter as tk
+
+server = xmlrpc.client.ServerProxy("http://localhost:8000/")
+card_states = [False] * 16
+
+def register_player():
+    player_name = player_name_entry.get()
+    if server.register_player(player_name):
+        register_button.config(state='disabled')
+        player_name_entry.config(state='disabled')
+    else:
+        print("Não foi possível registrar o jogador.")
+
+def check_game_state():
+    game_state = server.get_state()
+    if len(game_state['matched_pairs']) == 8:  # Todos os pares foram encontrados
+        winner = max(game_state['players'], key=game_state['players'].get)
+        print(f"Jogo terminado! O vencedor é {winner} com {game_state['players'][winner]} pares encontrados.")
+        root.quit()
+
+def reveal_card(card_index, card_value):
+    card_states[card_index] = True
+    cards[card_index].config(text=str(card_value))
+
+def play_card(card_index):
+    if not card_states[card_index]:
+        player_name = player_name_entry.get()
+        card_value, last_two_cards, hide_cards = server.play_card(player_name, card_index)  # Obtendo hide_cards
+        if card_value is not None:
+            reveal_card(card_index, card_value)
+            if hide_cards:  # Se hide_cards for True, esconda as duas últimas cartas após um breve intervalo
+                root.after(1000, hide_last_two_cards, last_two_cards)
+
+def hide_last_two_cards(last_two_cards):
+    for card_index, _ in last_two_cards:
+        cards[card_index].config(text=" ")  # Escondendo a carta
+        card_states[card_index] = False  # Verificar o estado do jogo após um intervalo
+
+def update_game_state():
+    game_state = server.get_state()
+    matched_pairs = game_state['matched_pairs']
+
+    for card_index, card_value in enumerate(game_state['cards']):
+        if card_index not in matched_pairs and not card_states[card_index]:
+            cards[card_index].config(text=" ")  # Assegura que as cartas não pareadas sejam escondidas
+        elif card_index in matched_pairs and not card_states[card_index]:
+            cards[card_index].config(text=str(card_value))  # Revela as cartas pareadas
+
+    root.after(1000, update_game_state)  # Atualiza o estado do jogo a cada 1 segundo
+
+
+
+def create_gui():
+    global player_name_entry, register_button, cards
+    cards = []
+
+    for i in range(16):
+        card_button = tk.Button(root, text=" ", width=8, height=4,
+                                command=lambda i=i: play_card(i))
+        card_button.grid(row=i // 4, column=i % 4)
+        cards.append(card_button)
+
+    player_name_label = tk.Label(root, text="Nome do Jogador")
+    player_name_label.grid(row=4, columnspan=4)
+
+    player_name_entry = tk.Entry(root)
+    player_name_entry.grid(row=5, columnspan=4)
+
+    register_button = tk.Button(root, text="Registrar", command=register_player)
+    register_button.grid(row=6, columnspan=4)
+    update_game_state()
+
+root = tk.Tk()
+root.title("Jogo da Memória")
+create_gui()
+root.mainloop()
---
+++ server.py
@@ -0,0 +1,104 @@
+from xmlrpc.server import SimpleXMLRPCServer
+import random
+import threading
+
+class MemoryGameServer:
+    def __init__(self):
+        self.cards = list(range(1, 9)) * 2
+        random.shuffle(self.cards)
+        self.players = {}
+        self.matched_pairs = []
+        self.current_player = None
+        self.player_list = []  # Lista de jogadores registrados
+        self.last_two_cards = []  # Armazena os índices das duas últimas cartas selecionadas
+
+    def register_player(self, player_name):
+        if len(self.players) < 2:
+            self.players[player_name] = 0
+            self.player_list.append(player_name)
+
+            if len(self.player_list) == 2:
+                # Inicialize as cartas e defina o primeiro jogador quando ambos os jogadores estiverem registrados
+                self.cards = list(range(1, 9)) * 2
+                random.shuffle(self.cards)
+                self.current_player = self.player_list[0]
+            return True
+        return False
+
+    def get_current_player(self):
+        return self.current_player
+
+    # def play_card(self, player_name, card_index):
+    #     if player_name == self.current_player and card_index not in [index for index, _ in self.last_two_cards]:
+    #         card_value = self.cards[card_index]
+
+    #         self.last_two_cards.append((card_index, card_value))
+
+    #         if len(self.last_two_cards) == 2:
+    #             index1, value1 = self.last_two_cards[0]
+    #             index2, value2 = self.last_two_cards[1]
+
+    #             if value1 == value2:
+    #                 self.players[player_name] += 1
+    #                 self.matched_pairs.extend([value1, value2])
+
+    #             # Limpando a lista das duas últimas cartas reveladas
+    #             self.last_two_cards.clear()
+
+    #             # Alternando o jogador
+    #             current_index = self.player_list.index(self.current_player)
+    #             next_index = (current_index + 1) % len(self.player_list)
+    #             self.current_player = self.player_list[next_index]
+
+    #         return card_value
+    #     return None
+
+    def play_card(self, player_name, card_index):
+        if player_name == self.current_player and card_index not in [index for index, _ in self.last_two_cards]:
+            card_value = self.cards[card_index]
+            self.last_two_cards.append((card_index, card_value))
+
+            hide_cards = False  # Inicializando a flag
+
+            if len(self.last_two_cards) == 2:
+                index1, value1 = self.last_two_cards[0]
+                index2, value2 = self.last_two_cards[1]
+
+                if value1 == value2:
+                    self.players[player_name] += 1
+                    self.matched_pairs.extend([index1, index2])
+                else:
+                    hide_cards = True  # Definindo a flag como True se as cartas não formarem um par
+
+                # Limpando a lista das duas últimas cartas reveladas após um intervalo, permitindo que os jogadores as vejam
+                timer = threading.Timer(1.0, self.clear_last_two_cards)  # Cria um timer de 1 segundo
+                timer.start()  # Inicia o timer
+
+                # Alternando o jogador
+                current_index = self.player_list.index(self.current_player)
+                next_index = (current_index + 1) % len(self.player_list)
+                self.current_player = self.player_list[next_index]
+
+            return card_value, self.last_two_cards, hide_cards  # Retornando a flag junto com os outros valores
+        return None, None, None
+
+    def clear_last_two_cards(self):
+        self.last_two_cards.clear()
+
+    def get_state(self):
+        return {
+            'cards': self.cards,
+            'players': self.players,
+            'matched_pairs': self.matched_pairs,
+            'current_player': self.current_player
+        }
+
+def main():
+    server = SimpleXMLRPCServer(('localhost', 8000), allow_none=True)
+    game_server = MemoryGameServer()
+    server.register_instance(game_server)
+    print("O servidor do jogo da memória está rodando na porta 8000...")
+    server.serve_forever()
+
+if __name__ == "__main__":
+    main()
